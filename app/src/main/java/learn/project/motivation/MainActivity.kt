45844a54223996ea70fceeb22289b2fdc0d0bf28
jeduardosa <jeduardosa86@gmail.com>
2022-05-22 11:06:41 -0300

    Project update

--- app/src/main/java/learn/project/motivation/MainActivity.kt
+++ app/src/main/java/learn/project/motivation/MainActivity.kt
@@ -2,10 +2,28 @@ package learn.project.motivation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import learn.project.motivation.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonNewPhrase.setOnClickListener(this)
+
+        supportActionBar?.hide()
+
+    }
+
+    override fun onClick(v: View?) {
+        if (v.id == R.id.button_new_phrase){
+
+        }
     }
 }
